40ffcf7df1c3b44923cb71c0a7e3ab2cf00af703
firthj <josh.firth@aucklandcouncil.govt.nz>
2022-03-01 14:30:43 +1300

    added dots to utils helper

--- R/utils-export.R
+++ R/utils-export.R
@@ -10,6 +10,6 @@ relative_change <- function(from, to) (to - from) / from
 #' be sourced.
 #'
 #' @export
-load_utils <- function(pattern = "") {
-  saucer::source_folder(file.path(get_root(), "R", "utils"), pattern = "")
+load_utils <- function(...) {
+  saucer::source_folder(file.path(get_root(), "R", "utils"), ...)
 }
